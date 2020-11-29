f84af31fd51e00baf17e16f1d16604fca6be50a1
Hubert Balcerzak <hubertbalc@gmail.com>
2020-11-29 20:22:53 +0100

    gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    id("application")
+    id("org.openjfx.javafxplugin").version("0.0.9")
 }
 
 group = "pl.edu.agh.monalisa"
@@ -9,12 +11,22 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClassName = "pl.edu.agh.monalisa.MonaLisaApplication"
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_14
     targetCompatibility = JavaVersion.VERSION_14
 }
 
+javafx {
+    version = "15.0.1"
+    modules = listOf("javafx.base", "javafx.controls", "javafx.fxml")
+}
+
 dependencies {
     testImplementation("junit", "junit", "4.12")
     implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.inject:guice:4.2.3")
 }
